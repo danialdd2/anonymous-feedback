9bf10f6d27a11ac8793fac0c45ab29af0a71ac99
danialdd2 <danialdavaminp@gmail.com>
2025-02-01 14:12:51 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4CAF50", // رنگ اصلی
+        secondary: "#FF5722", // رنگ ثانویه
+      },
+      fontFamily: {
+        sans: ['"Poppins"', 'sans-serif'], // فونت‌های ترند
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      blur: {
+        xs: '2px',
+      },
+    },
   },
   plugins: [],
-};
+}
